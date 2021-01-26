a1ac815228fd9fc179ddc75e22010a2199a667d4
Lyle Rallos <61070788+dubz-skrrrt@users.noreply.github.com>
2021-01-26 18:50:58 +0800

    Movement Fixed

--- Pacman/Assets/Scripts/PlayerController.cs
+++ Pacman/Assets/Scripts/PlayerController.cs
@@ -4,34 +4,79 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    public float playerSpeed = 25f;
-    private float gravityValue = -9.81f;
 
+    public float MovementSpeed = 0f;
 
-    private void Start()
-    {
-        controller = gameObject.AddComponent<CharacterController>();
+    private Vector3 up = Vector3.zero,
+                    right = new Vector3(0,90,0),
+                    down = new Vector3(0,180,0),
+                    left = new Vector3(0,270,0),
+                    currentDirection = Vector3.zero;
+
+    private Vector3 initialPos = Vector3.zero;
+
+    void Start(){
+        QualitySettings.vSyncCount = 0;
+
+        initialPos = transform.position;
+
+        Reset();
     }
 
-    void Update()
-    {
-        if (playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
+    void Update(){
+        var isMoving = true;
+
+        if (Input.GetKey(KeyCode.W)){
+            currentDirection = up;
+        }else if (Input.GetKey(KeyCode.D)){
+            currentDirection = right;
+        }else if (Input.GetKey(KeyCode.S)){
+            currentDirection = down;
+        }else if (Input.GetKey(KeyCode.A)){
+            currentDirection = left;
+        }else{
+            isMoving = false;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        transform.localEulerAngles = currentDirection;
 
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
+        if(isMoving){
+            transform.Translate(Vector3.forward*MovementSpeed*Time.deltaTime);
         }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
     }
+
+    public void Reset(){
+        transform.position = initialPos;
+        currentDirection = down;
+    }
+    // private CharacterController controller;
+    // private Vector3 playerVelocity;
+    // public float playerSpeed = 25f;
+    // private float gravityValue = -9.81f;
+
+
+    // private void Start()
+    // {
+    //     controller = gameObject.AddComponent<CharacterController>();
+    // }
+
+    // void Update()
+    // {
+    //     if (playerVelocity.y < 0)
+    //     {
+    //         playerVelocity.y = 0f;
+    //     }
+
+    //     Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    //     controller.Move(move * Time.deltaTime * playerSpeed);
+
+    //     if (move != Vector3.zero)
+    //     {
+    //         gameObject.transform.forward = move;
+    //     }
+
+    //     playerVelocity.y += gravityValue * Time.deltaTime;
+    //     controller.Move(playerVelocity * Time.deltaTime);
+    // }
 
 }
